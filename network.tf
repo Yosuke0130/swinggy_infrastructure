4554efd7f83087def33f70314ca424c400ac73a3
yosuke.koma <yosuke.5pit2@gmail.com>
2021-07-02 19:55:00 +0900

    first commit

---
+++ network.tf
@@ -0,0 +1,123 @@
+# ------------------------
+# VPC
+# ------------------------
+resource "aws_vpc" "vpc" {
+  cidr_block                       = "172.0.0.0/16"
+  instance_tenancy                 = "default"
+  enable_dns_support               = true
+  enable_dns_hostnames             = true
+  assign_generated_ipv6_cidr_block = false
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-vpc"
+    project = var.project
+    Env     = var.environment
+  }
+}
+
+# ------------------------
+# Subnet
+# ------------------------
+resource "aws_subnet" "public_subnet_bastion" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "172.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-public-subnet-bastion"
+    project = var.project
+    Env     = var.environment
+    type    = "public"
+  }
+}
+
+resource "aws_subnet" "private_subnet_api" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "172.0.2.0/24"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-public-subnet-api"
+    project = var.project
+    Env     = var.environment
+    type    = "private"
+  }
+}
+
+resource "aws_subnet" "private_subnet_db" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "172.0.3.0/24"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-public-subnet-db"
+    project = var.project
+    Env     = var.environment
+    type    = "private"
+  }
+}
+
+# ------------------------
+# Root Table
+# ------------------------
+resource "aws_route_table" "public_rt_bastion" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name    = "${var.project}-${var.environment}-public-rt-bastion"
+    project = var.project
+    Env     = var.environment
+    type    = "public"
+  }
+}
+resource "aws_route_table_association" "pubic_rt_bastion" {
+  route_table_id = aws_route_table.public_rt_bastion.id
+  subnet_id      = aws_subnet.public_subnet_bastion.id
+}
+
+resource "aws_route_table" "private_rt_api" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name    = "${var.project}-${var.environment}-private-rt-api"
+    project = var.project
+    Env     = var.environment
+    type    = "private"
+  }
+}
+resource "aws_route_table_association" "private_rt_api" {
+  route_table_id = aws_route_table.private_rt_api.id
+  subnet_id      = aws_subnet.private_subnet_api.id
+}
+
+resource "aws_route_table" "private_rt_db" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name    = "${var.project}-${var.environment}-private-rt-db"
+    project = var.project
+    Env     = var.environment
+    type    = "private"
+  }
+}
+resource "aws_route_table_association" "private_rt_db" {
+  route_table_id = aws_route_table.private_rt_db.id
+  subnet_id      = aws_subnet.private_subnet_db.id
+}
+
+# ------------------------
+# Internet Gateway
+# ------------------------
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name    = "${var.project}-${var.environment}-igw"
+    project = var.project
+    Env     = var.environment
+  }
+}
+resource "aws_route" "public_rt_igw_r" {
+  route_table_id         = aws_route_table.public_rt_bastion.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
---
+++ security_group.tf
@@ -0,0 +1,154 @@
+# ------------------------
+# Security Group
+# ------------------------
+# bastion security group
+resource "aws_security_group" "bastion_sg" {
+  name        = "${var.project}-${var.environment}-bastion-sg"
+  description = "security group for bastion server"
+  vpc_id      = aws_vpc.vpc.id
+  tags = {
+    Name    = "${var.project}-${var.environment}-bastion-sg"
+    project = var.project
+    Env     = var.environment
+  }
+}
+resource "aws_security_group_rule" "bastion_in_http" {
+  security_group_id = aws_security_group.bastion_sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 80
+  to_port           = 80
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+resource "aws_security_group_rule" "bastion_in_https" {
+  security_group_id = aws_security_group.bastion_sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+resource "aws_security_group_rule" "web_out_http" {
+  security_group_id        = aws_security_group.bastion_sg.id
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = 80
+  to_port                  = 80
+  source_security_group_id = aws_security_group.api_sg.id
+}
+resource "aws_security_group_rule" "web_out_https" {
+  security_group_id        = aws_security_group.bastion_sg.id
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = 443
+  to_port                  = 443
+  source_security_group_id = aws_security_group.api_sg.id
+}
+resource "aws_security_group_rule" "web_in_ssh" {
+  security_group_id        = aws_security_group.bastion_sg.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 22
+  to_port                  = 22
+  source_security_group_id = ["0.0.0.0/0"]
+}
+
+# api security group
+resource "aws_security_group" "api_sg" {
+  name        = "${var.project}-${var.environment}-api-sg"
+  description = "security group for api server"
+  vpc_id      = aws_vpc.vpc.id
+  tags = {
+    Name    = "${var.project}-${var.environment}-api-sg"
+    project = var.project
+    Env     = var.environment
+  }
+}
+resource "aws_security_group_rule" "api_in_http" {
+  security_group_id        = aws_security_group.api_sg.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 80
+  to_port                  = 80
+  source_security_group_id = [aws_security_group.alb_sg.id,aws_security_group.bastion_sg.id]
+}
+resource "aws_security_group_rule" "api_in_https" {
+  security_group_id        = aws_security_group.api_sg.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 443
+  to_port                  = 443
+  source_security_group_id = [aws_security_group.alb_sg.id,aws_security_group.bastion_sg.id]
+}
+resource "aws_security_group_rule" "api_out_tcp3306" {
+  security_group_id        = aws_security_group.api_sg.id
+  type                     = "enngress"
+  protocol                 = "tcp"
+  from_port                = 3306
+  to_port                  = 3306
+  source_security_group_id = aws_security_group.db_sg.id
+}
+
+# db security group
+resource "aws_security_group" "db_sg" {
+  name        = "${var.project}-${var.environment}-db-sg"
+  description = "security group for db server"
+  vpc_id      = aws_vpc.vpc.id
+  tags = {
+    Name    = "${var.project}-${var.environment}-db-sg"
+    project = var.project
+    Env     = var.environment
+  }
+}
+resource "aws_security_group_rule" "db_in_dcp3306" {
+  security_group_id        = aws_security_group.db_sg.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 3306
+  to_port                  = 3306
+  source_security_group_id = aws_security_group.api_sg.id
+}
+
+# alb security group
+resource "aws_security_group" "alb_sg" {
+  name        = "${var.project}-${var.environment}-alb-sg"
+  description = "security group for alb"
+  vpc_id      = aws_vpc.vpc.id
+  tags = {
+    Name    = "${var.project}-${var.environment}-alb-sg"
+    project = var.project
+    Env     = var.environment
+  }
+}
+resource "aws_security_group_rule" "alb_in_http" {
+  security_group_id        = aws_security_group.alb_sg.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 80
+  to_port                  = 80
+  source_security_group_id = ["0.0.0.0/0"]
+}
+resource "aws_security_group_rule" "alb_in_https" {
+  security_group_id        = aws_security_group.alb_sg.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 443
+  to_port                  = 443
+  source_security_group_id = ["0.0.0.0/0"]
+}
+resource "aws_security_group_rule" "alb_out_http" {
+  security_group_id        = aws_security_group.alb_sg.id
+  type                     = "engress"
+  protocol                 = "tcp"
+  from_port                = 80
+  to_port                  = 80
+  source_security_group_id = aws_security_group.api_sg.id
+}
+resource "aws_security_group_rule" "alb_out_https" {
+  security_group_id        = aws_security_group.alb_sg.id
+  type                     = "engress"
+  protocol                 = "tcp"
+  from_port                = 443
+  to_port                  = 443
+  source_security_group_id = aws_security_group.api_sg.id
+}
